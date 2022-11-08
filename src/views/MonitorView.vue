092a42d94380a5d85ccebc39f1f0f31e1ed90560
Jinxiao <jyu25@vols.utk.edu>
2022-11-07 22:57:46 -0800

    add monitor view

---
+++ src/views/MonitorView.vue
@@ -0,0 +1,18 @@
+<template>
+    <iframe src="http://192.9.224.155:3000/d/-cdKDm9nk/nexres?orgId=1&refresh=5s&kiosk" width=auto height=auto frameborder="0"></iframe>
+    
+</template>
+
+<script>
+    export default{
+        beforeCreate: function() {
+            document.body.className = 'monitorview';
+        }
+    };
+</script>
+
+<style scoped>
+    .no-margin {
+        margin-top: 10px;
+    }
+</style>
