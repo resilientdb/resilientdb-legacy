0cc59ac6327d11e79d250f7cacb2d57cbcd05362
Jinxiao <jyu25@vols.utk.edu>
2022-12-04 09:55:01 -0800

    fix depoly.sh

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:nexres/nexres.github.io.git main:nexres-gh-pages
+git push -f git@github.com:nexres/nexres.github.io.git
 cd -
